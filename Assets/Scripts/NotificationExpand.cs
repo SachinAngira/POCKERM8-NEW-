193ddf179ac5e9aed729121ca756d715ca3293da
SachinAngira <msddirtbags@gmail.com>
2020-06-23 21:53:54 +0530

    Initial push

---
+++ Assets/Scripts/NotificationExpand.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationExpand : MonoBehaviour
+{
+    public RectTransform RT,holder;
+    public GameObject Srinkbtn, ExpandBtn;
+    public Text TXT;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSrink()
+    {
+     //   RT.sizeDelta = new Vector2(RT.sizeDelta.x,RT.sizeDelta.y - 178f);
+        holder.sizeDelta = new Vector2(RT.sizeDelta.x, 274f);
+        ExpandBtn.SetActive(true);
+        Srinkbtn.SetActive(false);
+         TXT.text  = " Pocker is any number of card games in which players wages over which hand is best according to that specific game's rulesin ways similar to these rankings";
+
+ 
+    }
+    public void OnExpend()
+    {
+        holder.sizeDelta = new Vector2(RT.sizeDelta.x, 434.3f);
+        ExpandBtn.SetActive(false);
+        Srinkbtn.SetActive(true);
+        TXT.text  = " Pocker is any number of card games in which players wages over which hand is best according to that specific game's rulesin ways similar to these rankings.                                                                                              dsdfsds dfsfs sdfs fh sgsd Sfsdf asdfa dgdsa sdfsadf sadfsad asdfasdf sadfsa sad sadfsadf sfasd fd v dfga asdc asfad asda a asdfas ";
+    }
+}
---
+++ Assets/Scripts/Web.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Web : MonoBehaviour
+{
+    void Start()
+    {
+        // A correct website page.
+        StartCoroutine(GetRequest("http://ec2-3-17-135-24.us-east-2.compute.amazonaws.com/api/auth/club"));
+
+     
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
